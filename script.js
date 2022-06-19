18fa171589a7c8bb5cd92d9e1051ecc54231e498
Vasily-N <101352346+Vasily-N@users.noreply.github.com>
2022-06-19 09:49:04 +0300

    refactor:

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const QuickSort = (a, left, right) => {
-  let i = left;
-  let j = right;
+  let i = left, j = right;
   const p = a[left];
   _reports.push(new Report(i, j, ReportType.Zone));
 
   while (i <= j) {
-      while (a[i] < p) {
-        i++;
-        _reports.push(new Report(i, j, ReportType.Select));
-      }
-      while (a[j] > p) {
-        j--;
-        _reports.push(new Report(i, j, ReportType.Select));
-      }
-      if (i > j) continue; 
-      if (a[i] != a[j]) {
-          [a[i], a[j]] = [a[j], a[i]];
-          _reports.push(new Report(i, j));
-      }
+    _reports.push(new Report(i, j, ReportType.Select));
 
+    while (a[i] < p) {
       i++;
+      _reports.push(new Report(i, j, ReportType.Select));
+    }
+
+    while (a[j] > p) {
       j--;
       _reports.push(new Report(i, j, ReportType.Select));
+    }
+
+    if (i > j) break;
+    if (a[i] != a[j]) {
+      [a[i], a[j]] = [a[j], a[i]];
+      _reports.push(new Report(i, j, ReportType.Sort));
+    }
+
+    i++; j--;
   }
 
   if (j > left) QuickSort(a, left, j);
@@ -32,7 +33,9 @@ const QuickSort = (a, left, right) => {
 
 const _block = document.getElementById('block');
 const _btnSort = document.getElementById('sort');
-let _arr, _arrDiv;
+let _arrDiv;
+
+let arr;
 const ARR_LENGTH = 30;
 
 const L_HEIGHT = 16; //todo: get from style
@@ -43,7 +46,6 @@ const L_MULT = 20;
 let animationTime = 1000; //todo: get from style
 let waitTime = 1000;
 
-let _reportsI;
 let _reports;
 
 const ReportType = {Sort : 0, Zone: 1, Select: 2}; //enum?
@@ -58,7 +60,6 @@ class Report {
 }
 
 const BtnSort = () => {
-  _reportsI = 0;
   _reports = [];
   _rZone = undefined;
   QuickSort(_arr, 0, _arr.length - 1);
@@ -94,11 +95,11 @@ const NextStep = () => {
     }
 
     ResetStyles();
-    if (_reportsI >= _reports.length) {
+    if (_reports.length == 0) {
       return; 
     }
 
-    const r = _reports[_reportsI++];
+    const r = _reports.shift();
     if (r.Type == ReportType.Zone) {
       _rZone = r;
     }
@@ -113,13 +114,16 @@ const NextStep = () => {
 
     if(_arrDiv[r.I]) _arrDiv[r.I].classList.add('active');
     if(_arrDiv[r.J]) _arrDiv[r.J].classList.add('active');
+
     if(r.Type != ReportType.Sort) {
-      setTimeout(NextStep, waitTime / 10  );
+      setTimeout(NextStep, waitTime / 10);
       return;
     }
+
     _arrDiv[r.I].style.top = `${GetPosition(r.J)}px`;
     _arrDiv[r.J].style.top = `${GetPosition(r.I)}px`;
-    [_arrDiv[r.I], _arrDiv[r.J]] = [_arrDiv[r.J], _arrDiv[r.I]]
+    [_arrDiv[r.I], _arrDiv[r.J]] = [_arrDiv[r.J], _arrDiv[r.I]];
+
     setTimeout(NextStep, animationTime + waitTime);
 }
 
